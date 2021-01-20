fffbc51205d0e1de2e820918942c26efa97f3686
Ashkan Ramezani <ashkan.ram70@gmail.com>
2021-01-19 20:53:48 -0800

    building up

--- client/src/pages/AdminVideo.js
+++ client/src/pages/AdminVideo.js
@@ -3,28 +3,48 @@ import API from "../utils/API";
 import { toast } from "react-toastify"
 import { Input, FormBtn } from "../components/Form";
 import 'react-toastify/dist/ReactToastify.css'
-import { Col, Row, Container, ColDark } from "../components/Grid";
+import { ColRow, Row, Container, ColDark } from "../components/Grid";
+import { Modal, Button, Form, Col } from 'react-bootstrap';
+
 import "./style.css"
 
 class AdminVideo extends Component {
     state = {
-       
+
     };
-     componentDidMount() {
-        
-     }
+    componentDidMount() {
+
+    }
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
             [name]: value
         });
     };
-  
-     
+
+
     render() {
         return (
             <Container fluid>
-               <p>hi</p>
+                <ColRow size="12">
+                    <Form>
+                        <Form.Row>
+                            <Col>
+                                <Form.Label>Video Name</Form.Label>
+                                <Form.Control onChange={this.handleInputChange} type="text" name="videoName" />
+                            </Col>
+                            <Col>
+                                <Form.Label>Video Description</Form.Label>
+                                <Form.Control onChange={this.handleInputChange} as="textarea" name="videoDescription" />
+                            </Col>
+                            <Col xs={2}>
+                                <Button onClick={this.saveNewService} variant="primary" type="submit">
+                                    Save
+                                        </Button>
+                            </Col>
+                        </Form.Row>
+                    </Form>
+                </ColRow>
             </Container>
         );
     }
