2fd30460793d193cdfcc9f358ace7495e388fde3
Ashkan Ramezani <ashkan.ram70@gmail.com>
2021-01-19 19:05:23 -0800

    menu added

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
+import {EmployeeMenu} from './components/Menu';
 /* PAGES */
 import Home from "./pages/Home";
+import AdminVideo from "./pages/AdminVideo";
 import NoMatch from "./pages/NoMatch";
 
 class App extends Component {
@@ -11,9 +13,11 @@ class App extends Component {
     return (
       <Router>
         <div>
+          <EmployeeMenu />
           <ToastContainer />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/admin-video" component={AdminVideo} />
             <Route component={NoMatch} />
           </Switch>
         </div>
--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -3,7 +3,35 @@ import API from "../../utils/API";
 import { toast } from "react-toastify";
 import "./style.css";
 
-export function LogOut() {
+export function EmployeeMenu(userId) {
+  return (
+    <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light bg-navBar">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul className="navbar-nav" id="category-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Videos
+              </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">All Videos</a>
+              <a class="dropdown-item" href="admin-video">Admin Video</a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/users">Users</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/profile">Profile</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+/* export function LogOut() {
   API.logOut()
     .then(res => {
       toast.success("You are successfully logged out");
@@ -35,8 +63,14 @@ export function AdminMenu(userInfo) {
         </button>
         <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
           <ul className="navbar-nav" id="category-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="/roles">Departments/Roles</a>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Videos
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">All Videos</a>
+                <a class="dropdown-item" href="#">Admin Video</a>
+              </div>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/servicesmenu">Services Menu</a>
@@ -129,3 +163,4 @@ export function CustomerMenu(userId) {
     </nav>
   )
 }
+ */
